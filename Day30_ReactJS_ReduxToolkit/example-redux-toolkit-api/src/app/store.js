6db1696420f6805f76f27294e362ce66bb1f8758
ducngtrung <ducnguyen.bme@gmail.com>
2023-03-04 11:43:57 +0700

    react

--- Day30_ReactJS_ReduxToolkit/example-redux-toolkit-api/src/app/store.js
+++ Day30_ReactJS_ReduxToolkit/example-redux-toolkit-api/src/app/store.js
@@ -1,6 +1,4 @@
 import { configureStore } from "@reduxjs/toolkit";
-// import { todolistApi } from "./services/todolist.service";
-// import todolistReducer from "./slices/todolist.slice";
 import todosReducer from "./slices/todoSlice";
 
 const store = configureStore({
