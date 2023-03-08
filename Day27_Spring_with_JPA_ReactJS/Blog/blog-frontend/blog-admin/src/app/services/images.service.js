f7bcaa65cdf3a4e8ab6fd7bdb04af75422dbc328
ducngtrung <ducnguyen.bme@gmail.com>
2023-03-08 21:12:00 +0700

    spring

--- Day27_Spring_with_JPA_ReactJS/Blog/blog-frontend/blog-admin/src/app/services/images.service.js
+++ Day27_Spring_with_JPA_ReactJS/Blog/blog-frontend/blog-admin/src/app/services/images.service.js
@@ -1,6 +1,5 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-// Define a service using a base URL and expected endpoints
 export const imageApi = createApi({
     reducerPath: "imageApi",
     baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:8080/api" }),
@@ -15,8 +14,7 @@ export const imageApi = createApi({
     }),
 });
 
-// Export hooks for usage in functional components, which are
-// auto-generated based on the defined endpoints
+
 export const {
     useUploadImageMutation
 } = imageApi;
--- Day27_Spring_with_JPA_ReactJS/Blog/blog-frontend/blog-user/src/App.jsx
+++ Day27_Spring_with_JPA_ReactJS/Blog/blog-frontend/blog-user/src/App.jsx
@@ -1,34 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { Route, Routes } from "react-router-dom";
+
+import Layout from "./components/layout/Layout";
+import NotFound from "./components/notfound/NotFound";
+import BlogDetail from "./pages/blog/BlogDetail";
+import Home from "./pages/home/Home";
+import Search from "./pages/search/Search";
+import TagDetail from "./pages/tag/TagDetail";
+import TagList from "./pages/tag/TagList";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    return (
+        <Routes>
+            <Route path="/" element={<Layout />}>
+                {/* Trang hiển thị danh sách tất cả bài viết */}
+                <Route index element={<Home />} />
+                
+                <Route path="/tags">
+                    {/* Trang hiển thị danh sách tất cả danh mục */}
+                    <Route index element={<TagList />} />
+                    {/* Trang hiển thị danh sách bài viết đang áp dụng danh mục tagName */}
+                    <Route path=":tagName" element={<TagDetail />} />
+                </Route>
+                
+                {/* Trang tìm kiếm */}
+                <Route path="/search" element={<Search />} />
+                
+                {/* Trang hiển thị chi tiết 1 bài viết */}
+                <Route path="/blogs/:blogId/:blogSlug" element={<BlogDetail />} />
+                
+                {/* Trang Not Found */}
+                <Route path="*" element={<NotFound />} />
+            </Route>
+        </Routes>
+    );
 }
 
-export default App
+export default App;
