96d0f2533ed0b9f19f0cd6e2004a37dfc2d0f485
Duc Nguyen <ducnguyen.bme@gmail.com>
2023-03-15 20:12:07 +0700

    react macos

--- Day27_Spring_with_JPA_ReactJS/Blog/blog-frontend/blog-admin/src/main.jsx
+++ Day27_Spring_with_JPA_ReactJS/Blog/blog-frontend/blog-admin/src/main.jsx
@@ -1,7 +1,7 @@
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle';
-import "easymde/dist/easymde.min.css";
+import 'easymde/dist/easymde.min.css';
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
--- Day27_Spring_with_JPA_ReactJS/Blog/blog-frontend/blog-user/src/main.jsx
+++ Day27_Spring_with_JPA_ReactJS/Blog/blog-frontend/blog-user/src/main.jsx
@@ -1,8 +1,9 @@
+// import "react-toastify/dist/ReactToastify.css";
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
-// import "react-toastify/dist/ReactToastify.css";
 import App from "./App";
 import store from "./app/store";
 import "./index.css";
