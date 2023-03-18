841e55df07e289fbf54abe9d43d5baffbaee94e3
Duc Nguyen <ducnguyen.bme@gmail.com>
2023-03-18 17:11:26 +0700

    react

--- Day27_Spring_with_JPA_ReactJS/Blog/blog-backend/src/main/java/techmaster/blogbackend/exception/NotFoundException.java
+++
@@ -1,7 +0,0 @@
-package techmaster.blogbackend.exception;
-
-public class NotFoundException extends RuntimeException{
-    public NotFoundException(String message) {
-        super(message);
-    }
-}
